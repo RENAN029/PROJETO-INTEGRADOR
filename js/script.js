b165dd8485089c6d37080f1e222f9ea610babb24
unknown <jmbcondados@gmail.com>
2024-12-19 04:23:20 -0300

    version 0.0.5 - 19/12

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Função que exibe um alert assim que a página é carregada
+window.onload = function () {
+    // Exibe a mensagem de boas-vindas
+    alert("Bem-vindo ao nosso site! Qualquer dúvida, nos consulte ou mande um e-mail para: matheus.belchior@escolar.ifrn.edu.br");
+  };  
