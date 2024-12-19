38300a609f4a0c485a95ec7e7f1270e4ad0e4d81
MatheusBelchior55 <jmbcondados@gmail.com>
2024-12-19 04:45:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-// Função que exibe um alert assim que a página é carregada
 window.onload = function () {
     // Exibe a mensagem de boas-vindas
     alert("Bem-vindo ao nosso site! Qualquer dúvida, nos consulte ou mande um e-mail para: matheus.belchior@escolar.ifrn.edu.br");
   };  
-=======
-console.log('Página Teste carregada com sucesso!');
-console.log('mais um teste')
->>>>>>> fca7c797a913a74091123b51d9fa8f749c7cff2b
